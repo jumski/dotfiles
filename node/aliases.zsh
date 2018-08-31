d8abf585297cbcc6297ae41091d3552798fb6b7b
Wojtek Majewski <jumski@gmail.com>
2018-08-31 20:36:51 +0200

    Add `ember` node alias

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,2 +1,3 @@
 alias unfuck_js="rm -rf tmp/ node_modules/ bower_components/ dist/ && bower cache clean && npm cache clean && npm install && bower install && mkdir -p dist"
 
+alias ember="./node_modules/.bin/ember"
