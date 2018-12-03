9c222e8d789886ea98ea6efe5545b79ce550a06f
Wojtek Majewski <jumski@gmail.com>
2018-12-03 23:27:52 +0100

    System: improve installation for bat

--- system/install.sh
+++ system/install.sh
@@ -7,9 +7,11 @@ sudo apt install -y \
   tig \
   xclip \
 
-wget -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v0.8.0/bat-musl_0.8.0_amd64.deb
-sudo dpkg -i /tmp/bat.deb
-rm /tmp/bat.deb
+which bat || (
+  wget -O /tmp/bat.deb https://github.com/sharkdp/bat/releases/download/v0.8.0/bat-musl_0.8.0_amd64.deb
+  sudo dpkg -i /tmp/bat.deb
+  rm /tmp/bat.deb
+)
 
 sudo ln -sf $ZSH/bin/chrum /usr/bin/chrum
 sudo chmod +x /usr/bin/chrum
