941ebb498a4fb68559ddb6f755532d758c22aa47
Wojtek Majewski <jumski@gmail.com>
2018-12-24 01:57:57 +0100

    Tmux/aliases.zsh: add `mxf` alias
    Alias uses FZF to filter available tuxinator projects and opents
    selected project on enter.

--- tmux/aliases.zsh
+++ tmux/aliases.zsh
@@ -1,1 +1,14 @@
 alias mux=tmuxinator
+
+function mxf() {
+  local preview_cmd='bat --number --paging=never --color=always /home/jumski/.tmuxinator/{}.yml'
+  local project_name=$(
+    ls ~/.tmuxinator/*.yml |
+    xargs -L1 basename -s .yml |
+    fzf --ansi -d 15 --preview="$preview_cmd"
+  )
+
+  tmuxinator $project_name
+}
+
+
