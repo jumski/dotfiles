1cdf16aeb2c766cada00f208494d01ab97b732f3
Wojtek Majewski <jumski@gmail.com>
2019-01-24 17:31:32 +0100

    various fixes

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,5 @@
 export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR" || true
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
