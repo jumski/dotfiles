2bf2cb1540ca11ec68ce76df450777f1389fc8a7
Wojtek Majewski <jumski@gmail.com>
2019-09-05 00:00:27 +0200

    python: enable asdf in install.sh

--- python/install.sh
+++ python/install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if [ ! -d ~/.pyenv ]; then
-  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-fi
-
-pyenv install --skip-existing 2.7.8
-pyenv install --skip-existing 3.6.4
+asdf plugin-install python
+asdf install python 3.7
