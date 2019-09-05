c131a75b7eceafe9908b2c1e5e38c548c8532497
Wojtek Majewski <jumski@gmail.com>
2019-09-06 00:09:47 +0200

    lenovo-y530: comment out all the stuff

--- lenovo-y530/install.sh
+++ lenovo-y530/install.sh
@@ -1,5 +1,5 @@
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt install -y nvidia-driver-415 nvidia-prime
+# sudo add-apt-repository ppa:graphics-drivers/ppa
+# sudo apt install -y nvidia-driver-415 nvidia-prime
 
 # echo /etc/modprobe.d/blacklist.conf | sudo tee /etc/modprobe.d/blacklist-lenovo.conf
 
