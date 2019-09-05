43b06a2834c86737f2ce62e9965c59378447dd80
Wojtek Majewski <jumski@gmail.com>
2019-09-05 22:09:50 +0200

    python: do not overload LD_LIBRARY_PATH for kratos

--- python/kratos.zsh
+++ python/kratos.zsh
@@ -1,3 +1,3 @@
 export KRATOS_ROOT=/home/jumski/installed/KratosRelease
-export LD_LIBRARY_PATH=${KRATOS_ROOT}/libs:${KRATOS_ROOT}/OpenMPI/lib:${LD_LIBRARY_PATH}
+# export LD_LIBRARY_PATH=${KRATOS_ROOT}/libs:${KRATOS_ROOT}/OpenMPI/lib:${LD_LIBRARY_PATH}
 export PYTHONPATH=${KRATOS_ROOT}:${KRATOS_ROOT}/libs:${PYTHONPATH}
