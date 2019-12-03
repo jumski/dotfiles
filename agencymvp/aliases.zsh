137ea59b186eb764b544341472c2606f5a42026e
Wojtek Majewski <jumski@gmail.com>
2019-12-03 13:51:26 +0100

    agencymvp: fix docker aliases

--- agencymvp/aliases.zsh
+++ agencymvp/aliases.zsh
@@ -1,3 +1,4 @@
-alias docker_rake="docker-compose run rails env PARALLEL=10 CLEAN_SETUP=1 bundle exec rake"
-alias docker_dbredo="docker_rake db:drop db:create 'db:restore[/app/tmp/dump.sql]' db:migrate"
+alias drake="docker-compose run --user=$(id -u ${USER}):$(id -g ${USER}) rails env bundle exec rake"
+alias dbe="docker-compose run --user=$(id -u ${USER}):$(id -g ${USER}) rails env bundle exec"
+alias ddbredo="docker-compose run --user=$(id -u ${USER}):$(id -g ${USER}) rails env PARALLEL=10 CLEAN_SETUP=1 bundle exec rake db:drop db:create 'db:restore[/app/tmp/dump.sql]' db:migrate"
 
