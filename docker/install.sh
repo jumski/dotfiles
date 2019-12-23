9e20138f0056dd919ac70d53148a4e09731fccc0
Wojtek Majewski <jumski@gmail.com>
2019-12-23 15:44:04 +0100

    docker: fix docker-compose installation

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo groupadd docker
-sudo usermod -aG docker jumski
+if ! which docker-compose 2>&1 >/dev/null; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo groupadd docker
+  sudo usermod -aG docker jumski
+fi
