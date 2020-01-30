b17cccb2ecfcc2c87e3dbf476fe927306cd6cdfe
Wojtek Majewski <jumski@gmail.com>
2020-01-30 18:35:12 +0100

    agencymvp: add and improve some aliases

--- agencymvp/aliases.zsh
+++ agencymvp/aliases.zsh
@@ -1,4 +1,5 @@
 alias drake="docker-compose run --user=$(id -u ${USER}):$(id -g ${USER}) rails env bundle exec rake"
 alias dbe="docker-compose run --user=$(id -u ${USER}):$(id -g ${USER}) rails env bundle exec"
-alias ddbredo="docker-compose run --user=$(id -u ${USER}):$(id -g ${USER}) rails env PARALLEL=10 CLEAN_SETUP=1 bundle exec rake db:drop db:create 'db:restore[/app/tmp/dump.sql]' db:migrate"
+alias ddbredo="docker-compose run --user=$(id -u ${USER}):$(id -g ${USER}) rails env PARALLEL=4 CLEAN_SETUP=1 bundle exec rake db:drop db:create 'db:restore[/app/tmp/dump.sql]' db:migrate"
+alias dpsql="docker-compose exec postgres psql -U postgres"
 
