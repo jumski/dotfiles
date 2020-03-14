d4823ffac41044cf0aff0b2f735e3af504caf292
Wojtek Majewski <jumski@gmail.com>
2020-03-14 13:39:33 +0100

    vim/install.sh: fix output redirection

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,6 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-if which yarn 2&>1 >/dev/null; then
+if which yarn 2>&1 >/dev/null; then
   yarn global add instant-markdown-d
 fi
