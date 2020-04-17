caa4341e198a77d6c7d0fa24efcebf4c853aae0d
Wojtek Majewski <jumski@gmail.com>
2020-04-17 15:51:11 +0200

    clojure: proper installation of cli version

--- clojure/install.sh
+++ clojure/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+if ! which clj 2>&1 >/dev/null; then
+  pushd /tmp
+    clojure_installer=linux-install-1.10.1.536.sh
+    curl -O https://download.clojure.org/install/$clojure_installer
+    chmod +x $clojure_installer
+    sudo ./$clojure_installer
+  popd
+fi
+
 yarn global add shadow-cljs
