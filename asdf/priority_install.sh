030aa0df848c029bd752a73f0ef1a1d56b280453
Wojtek Majewski <jumski@gmail.com>
2020-06-08 15:26:39 +0200

    Add fish and pacman installs

--- asdf/priority_install.sh
+++ asdf/priority_install.sh
@@ -4,6 +4,8 @@ which asdf 2>/dev/null 1>/dev/null || {
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.1
 }
 
+source ~/.asdf.sh
+
 # Install all asdf plugins
 for plugin_name in $(cut -d' ' -f1 tool-versions.symlink | tr '\n' ' '); do
   asdf plugin-add $plugin_name
@@ -11,6 +13,9 @@ done
 
 # import PGP keys for node
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+# make sure we have top-level .tool-versions linked
+ln `realpath tool-versions.symlink` $HOME/.tool-versions
 
 # install all versions from tool-versions.symlink
 asdf install
