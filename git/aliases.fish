6c868b8b3dc956ed8baff1ebbe6ae3945cdd734e
Wojtek Majewski <jumski@gmail.com>
2020-06-18 14:24:14 +0200

    uncommit git aliases, add discord

--- git/aliases.fish
+++ git/aliases.fish
@@ -13,13 +13,13 @@ alias g=git
 #alias gp='git push origin HEAD'
 #
 ## Remove `+` and `-` from start of diff lines; just rely upon color.
-#alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 #
-#alias gc='git commit'
+alias gc='git commit'
 #alias gca='git commit -a'
 #alias gco='git checkout'
 #alias gcb='git copy-branch-name'
-#alias gb='git branch -vv'
+alias gb='git branch -vv'
 ## alias gbb='git branch -vv'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 #alias gac='git add -A && git commit -m'
@@ -31,11 +31,11 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 #alias gdiff='git diff --color-words=.'
 #compdef _git gdiff='git-diff'
 #
-#alias gapan='git add --intent-to-add . && git add --patch'
-#alias gap='git add --intent-to-add . && git add --patch'
+alias gapan='git add --intent-to-add . && git add --patch'
+alias gap='git add --intent-to-add . && git add --patch'
 #compdef _git gapan='git-add'
 #compdef _git gap='git-add'
 #
 #alias gcob='git checkout $(git-branch-fzf --sort=-committerdate)'
 #alias gcoba='git checkout $(git-branch-fzf --sort=-committerdate --all)'
-#alias gbf=git-branch-fzf
+alias gbf=git-branch-fzf
