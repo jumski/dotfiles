18691666096ae9384e7d7a80aa32c286536ef59f
Wojtek Majewski <jumski@gmail.com>
2020-07-06 13:29:09 +0200

    add dotbot and colortheme for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,4 @@
 
-#source aliases.fish
-
 set -U DOTFILES_PATH "$HOME/.dotfiles"
 
 # load the _path.fish files first
@@ -9,7 +7,12 @@ for file in $DOTFILES_PATH/**/_path.fish
 end
 
 # load other files later
-set files_to_load (find -name '*.fish' | grep -v fish/config.fish | grep -v _path.fish)
+set files_to_load (find $DOTFILES_PATH -name '*.fish' | grep -v fish/config.fish | grep -v _path.fish)
 for file in $files_to_load
   source $file
 end
+
+theme_gruvbox dark
+
+# remove greeting
+set fish_greeting
