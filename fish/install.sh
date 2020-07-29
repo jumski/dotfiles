85893a902621b6a64ecb7edb04ac8f6dd77e5a19
Wojtek Majewski <jumski@gmail.com>
2020-07-29 12:13:26 +0200

    move fisher install from dotbot to fish/install.sh

--- fish/install.sh
+++ fish/install.sh
@@ -4,3 +4,11 @@ if !which fish &>/dev/null; then
   chsh -s $(which fish)
 fi
 
+if which fisher &>/dev/null; then
+  echo Updating fisher
+  fisher self-update
+else
+  echo Installing fisher
+  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fi
+
