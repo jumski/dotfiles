56458bbe245b55897d0b092bd6f426d032dc2cde
Wojtek Majewski <jumski@gmail.com>
2020-09-08 09:00:04 +0200

    commit small changes

--- fonts/install.sh
+++ fonts/install.sh
@@ -3,6 +3,7 @@
 destination_path=/usr/share/fonts/truetype/ttf-monaco/Monaco_Linux.ttf
 
 if [ ! -f $destination_path ]; then
+  sudo mkdir -p $(dirname $destination_path)
   sudo cp fonts/Monaco_Linux.ttf $destination_path &&
     sudo fc-cache -fv
 fi
