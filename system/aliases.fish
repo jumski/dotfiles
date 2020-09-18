c9f4525aa2e759a60090d3941dd4bfb496581105
Wojtek Majewski <jumski@gmail.com>
2020-09-18 08:52:57 +0200

    system: add speedtest-bits and speedtest-bytes aliases

--- system/aliases.fish
+++ system/aliases.fish
@@ -8,3 +8,6 @@ if which gls &>/dev/null
 
   alias la="gls -A --color"
 end
+
+alias speedtest-bytes="wget -O /dev/null http://speedtest.tele2.net/10GB.zip"
+alias speedtest-bits="wget -O /dev/null http://speedtest.tele2.net/10GB.zip --report-speed=bits"
