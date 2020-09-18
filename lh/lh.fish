ff4c93bf5ad0cc41e03e25634a87a5119a355833
Wojtek Majewski <jumski@gmail.com>
2020-09-18 08:52:48 +0200

    lh: update aliases

--- lh/lh.fish
+++ lh/lh.fish
@@ -1,12 +1,19 @@
-# run application/stop application
+## Run application/stop application
 alias lhu="docker-compose -f docker-compose.yml -f docker-compose.app.yml up"
-alias lhd="docker-compose -f docker-compose.yml -f docker-compose.app.yml down"
+alias lhd="docker-compose -f docker-compose.yml -f docker-compose.app.yml down --remove-orphans"
 
-# same as above, but also run all services this service depends on
+## Force rebuild
+alias lhr="docker-compose -f docker-compose.yml -f docker-compose.app.yml down --rmi local"
+
+## Same as above, but also run all services this service depends on
 alias lhus="docker-compose -f docker-compose.yml -f docker-compose.app.yml -f docker-compose.services.yml up"
-alias lhds="docker-compose -f docker-compose.yml -f docker-compose.app.yml -f docker-compose.services.yml down"
+alias lhds="docker-compose -f docker-compose.yml -f docker-compose.app.yml -f docker-compose.services.yml down --remove-orphans"
+
+# rebuild
+alias lhb="docker-compose -f docker-compose.yml -f docker-compose.app.yml build"
 
 # attach bash shell to first running docker container (the app)
 function lhe
     docker exec -it  (docker ps | grep -m 1 start_docker | awk '{print $1}') /bin/bash
 end
+
