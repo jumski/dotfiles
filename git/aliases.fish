25ca63fb16d78ca69292e84d54620861619a0f6e
Wojtek Majewski <jumski@gmail.com>
2020-09-29 19:13:49 +0200

    git: sort out aliases, convert some to abbreviations

--- git/aliases.fish
+++ git/aliases.fish
@@ -1,10 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-if which hub &>/dev/null
-  alias git=hub
-end
-
-alias g=git
 
 
 # The rest of my fun git aliases
@@ -15,13 +8,10 @@ alias g=git
 ## Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 #
-alias gc='git commit'
 # alias gca='git commit -a'
 # alias gco='git checkout'
 # alias gcb='git copy-branch-name'
-alias gb='git branch -vv'
 ## alias gbb='git branch -vv'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 #alias gac='git add -A && git commit -m'
 #alias ge='git-edit-new'
 #
@@ -31,11 +21,6 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 #alias gdiff='git diff --color-words=.'
 #compdef _git gdiff='git-diff'
 #
-alias gapan='git add --intent-to-add . && git add --patch'
-alias gap='git add --intent-to-add . && git add --patch'
 #compdef _git gapan='git-add'
 #compdef _git gap='git-add'
 #
-alias gcob='git checkout (git-branch-fzf --sort=-committerdate)'
-alias gcoba='git checkout (git-branch-fzf --sort=-committerdate --all)'
-alias gbf=git-branch-fzf
