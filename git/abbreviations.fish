d602fda71b6854af9a4ffff39902b3b66af6251e
Wojtek Majewski <jumski@gmail.com>
2020-09-29 19:17:38 +0200

    git: add abbreviations

---
+++ git/abbreviations.fish
@@ -0,0 +1,10 @@
+
+abbr --add g git
+abbr --add gc 'git commit'
+abbr --add gb 'git branch -vv'
+abbr --add gs 'git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+abbr --add gapan 'git add --intent-to-add . && git add --patch'
+abbr --add gap 'git add --intent-to-add . && git add --patch'
+abbr --add gcob 'git checkout (git-branch-fzf --sort=-committerdate)'
+abbr --add gcoba 'git checkout (git-branch-fzf --sort=-committerdate --all)'
+abbr --add gbf git-branch-fzf
