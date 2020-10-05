ab9c5fad542e7125e7378212cd490ed848716520
Wojtek Majewski <jumski@gmail.com>
2020-10-05 20:26:33 +0200

    git: convert abbreviations back to functions with --wraps

--- git/aliases.fish
+++ git/aliases.fish
@@ -1,4 +1,47 @@
 
+alias g=git
+
+function gc --wraps "git commit"
+  git commit $argv
+end
+
+function gb --wraps "git branch -vv"
+  git branch -vv $argv
+end
+
+function gs --wraps "git status -sb"
+  git status -sb $argv
+end
+
+function gap --wraps "git add --patch"
+  git add --intent-to-add . && git add --patch $argv
+end
+
+function gcob
+  set branch (git-branch-fzf --sort=-committerdate)
+  git checkout $branch
+end
+
+function gcoba
+  set branch (git-branch-fzf --sort=-committerdate --all)
+  git checkout $branch
+end
+
+alias gbf=git-branch-fzf
+
+function gd --wraps "git diff"
+  git diff --color $argv | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r
+end
+
+function gwip
+  git add -A
+  git rm (git ls-files --deleted) 2> /dev/null
+  git commit --no-verify -m "--wip-- [skip ci]"
+end
+
+function gunwip
+  git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1
+end
 
 # The rest of my fun git aliases
 #alias gl='git pull --prune'
@@ -6,7 +49,6 @@
 #alias gp='git push origin HEAD'
 #
 ## Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 #
 # alias gca='git commit -a'
 # alias gco='git checkout'
@@ -15,12 +57,4 @@ alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 #alias gac='git add -A && git commit -m'
 #alias ge='git-edit-new'
 #
-#alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip-- [skip ci]"'
-#alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
-#
 #alias gdiff='git diff --color-words=.'
-#compdef _git gdiff='git-diff'
-#
-#compdef _git gapan='git-add'
-#compdef _git gap='git-add'
-#
