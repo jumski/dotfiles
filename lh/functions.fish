0c06f79e6c42a9792fc4a7aac4a4d8a92ea8b030
Wojtek Majewski <jumski@gmail.com>
2020-10-08 09:16:28 +0200

    lh: improve mapping for muxing into services

--- lh/functions.fish
+++ lh/functions.fish
@@ -5,7 +5,9 @@ function etvrepo
 end
 
 function lhservice
+  set services_path ~/work/lh/lh-be-connected-factories/services/
   set -l service_path
-  set service_path (find ~/work/lh/lh-be-connected-factories/services/* -type d -maxdepth 0 -print 2> /dev/null | fzf +m) &&
-  muxit $service_path
+  set service_name (find ~/work/lh/lh-be-connected-factories/services/* -maxdepth 0 -type d -exec basename {} \; 2>/dev/null | fzf +m --preview "echo {}; echo ---------; ls --color=always $services_path/{}") &&
+
+  muxit $services_path/$service_name
 end
