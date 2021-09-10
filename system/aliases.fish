2c59049644f745038a87e26b692d6019ecfe4539
Wojtek Majewski <jumski@gmail.com>
2021-09-10 21:54:33 +0200

    system: add calc alias to dc

--- system/aliases.fish
+++ system/aliases.fish
@@ -11,3 +11,5 @@ end
 
 alias speedtest-bytes="wget -O /dev/null http://speedtest.tele2.net/10GB.zip"
 alias speedtest-bits="wget -O /dev/null http://speedtest.tele2.net/10GB.zip --report-speed=bits"
+
+alias calc=/usr/bin/dc
