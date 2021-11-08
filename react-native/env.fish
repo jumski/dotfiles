bbeee9752dcc38189310b86fbe450981fd1340dc
Wojtek Majewski <jumski@gmail.com>
2021-11-08 14:43:07 +0100

    react-native: add env.fish

---
+++ react-native/env.fish
@@ -0,0 +1,5 @@
+#!/bin/fish
+set --export ANDROID_SDK /home/jumski/Android/Sdk
+set --export PATH "$PATH:/home/jumski/Android/Sdk/platform-tools"
+set --export PATH "$PATH:/home/jumski/Android/Sdk/emulator"
+# set --export PATH "$PATH:/home/jumski/Android/Sdk/tools"
