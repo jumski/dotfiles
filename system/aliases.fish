7cb6291e24d8f8372c4de4565d6f425850ec9d83
Wojtek Majewski <jumski@gmail.com>
2022-06-22 00:10:58 +0200

    system: add ls alias to exa

--- system/aliases.fish
+++ system/aliases.fish
@@ -1,14 +1,5 @@
 # grc overides for ls
-if which gls &>/dev/null
-  alias ls="gls -F --color"
-
-  alias l="gls -lAh --color"
-
-  alias ll="gls -l --color"
-
-  alias la="gls -A --color"
-end
-
+alias ls 'exa -a'
 alias speedtest-bytes="wget -O /dev/null http://speedtest.tele2.net/10GB.zip"
 alias speedtest-bits="wget -O /dev/null http://speedtest.tele2.net/10GB.zip --report-speed=bits"
 
