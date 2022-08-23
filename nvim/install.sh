61a1c41b2794f27bc2c2eeddbf36b43cf4d88897
Wojtek Majewski <jumski@gmail.com>
2022-08-23 18:04:36 +0200

    nvim: working packer with some tpope plugins and theme

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! test -f $HOME/.local/share/nvim/site/autoload/plug.vim; then
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+if ! test -f ~/.local/share/nvim/site/pack/packer/start/packer.nvim; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
---
+++ nvim/lua/plugins/packer.lua
@@ -0,0 +1,44 @@
+local cmd = vim.cmd
+local packer = require 'packer'
+
+cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- tpope
+  use 'tpope/vim-abolish'
+  use 'tpope/vim-bundler'
+  use 'tpope/vim-capslock'
+  use 'tpope/vim-classpath'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-dispatch'
+  use 'tpope/vim-dotenv'
+  use 'tpope/vim-endwise'
+  use 'tpope/vim-eunuch'
+  use 'tpope/vim-fireplace'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-git'
+  use 'tpope/vim-haml'
+  use 'tpope/vim-markdown'
+  use 'tpope/vim-obsession'
+  use 'tpope/vim-projectionist'
+  use 'tpope/vim-rails'
+  use 'tpope/vim-rake'
+  use 'tpope/vim-rhubarb'
+
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-sexp-mappings-for-regular-people'
+  use 'tpope/vim-sleuth'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-unimpaired'
+  use 'tpope/vim-vinegar'
+
+  -- styles / visuals
+  use {"ellisonleao/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+end)
