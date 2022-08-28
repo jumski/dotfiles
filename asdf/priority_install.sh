d09d2504cc4e8c65f0c48f63918251fcbede8024
Wojciech Majewski <wojciech.majewski@pm.me>
2022-08-28 13:03:32 +0200

    asdf: remove gpg keys fetch from priority_install.sh

--- asdf/priority_install.sh
+++ asdf/priority_install.sh
@@ -11,8 +11,8 @@ for plugin_name in $(cut -d' ' -f1 tool-versions.symlink | tr '\n' ' '); do
   asdf plugin add $plugin_name
 done
 
-# import PGP keys for node
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf update
+asdf plugin update --all
 
 # make sure we have top-level .tool-versions linked
 ln --force `realpath tool-versions.symlink` $HOME/.tool-versions
