37236b76bade3dfc02453991fad78d19c58f6e83
Wojtek Majewski <jumski@gmail.com>
2022-10-12 10:55:52 +0200

    nvim/install.sh - fix

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if ! test -f ~/.local/share/nvim/site/pack/packer/start/packer.nvim; then
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+packer_dir="~/.local/share/nvim/site/pack/packer/start/packer.nvim"
+
+if ! test -d $packer_dir; then
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_dir
 fi
