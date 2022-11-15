70734e1f56b57091c66ccbeaf3689b1382455872
Wojciech Majewski <wojciech.majewski@pm.me>
2022-11-15 14:54:02 +0100

    fonts: add monaco from gajewsky

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-destination_path=/usr/share/fonts/truetype/ttf-monaco/Monaco_Linux.ttf
+destination_path=/usr/share/fonts/truetype/ttf-monaco/
 
 if [ ! -f $destination_path ]; then
-  sudo mkdir -p $(dirname $destination_path)
-  sudo cp fonts/Monaco_Linux.ttf $destination_path &&
+  sudo mkdir -p $destination_path
+  sudo cp fonts/*.{ttf,otf} $destination_path &&
     sudo fc-cache -fv
 fi
