2a95d8c6a79b5d0902441aec9800f7112e020d59
Wojciech Majewski <wojciech.majewski@pm.me>
2023-04-19 20:20:53 +0200

    nvim: disable solargraph for now

--- nvim/lua/plugins/lspcontainers.lua
+++ nvim/lua/plugins/lspcontainers.lua
@@ -1,4 +1,4 @@
-require'lspconfig'.solargraph.setup {
-  cmd = require'lspcontainers'.command('solargraph');
-}
+-- require'lspconfig'.solargraph.setup {
+--   cmd = require'lspcontainers'.command('solargraph');
+-- }
 
