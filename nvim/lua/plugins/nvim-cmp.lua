c921a983c8cd87bb312f85dae20819c93cf9da2c
Wojciech Majewski <wojciech.majewski@pm.me>
2023-04-19 20:20:53 +0200

    nvim: disable solargraph for now

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -69,6 +69,6 @@ cmp.setup.cmdline(':', {
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['solargraph'].setup {
-  capabilities = capabilities
-}
+-- require('lspconfig')['solargraph'].setup {
+--   capabilities = capabilities
+-- }
