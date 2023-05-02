4938d7ffcff1936c3e7e972f41314e4db32c18ee
Wojciech Majewski <wojciech.majewski@pm.me>
2023-05-02 11:00:58 +0200

    nvim: Add vim-ai mappings to mappings.lua.

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,3 +11,22 @@ map('n', '<leader>f', ':tabn<CR>', default_opts)
 map('n', '<leader>x', ':tabclose<CR>', default_opts)
 map('n', '<C-s>', ':w<CR>', default_opts)
 map('i', '<C-s>', '<Esc>:w<CR>', default_opts)
+
+-- vim-ai mappings
+vim_ai_opts = {noremap = true}
+
+-- :AIChat
+map('n', '<leader>d', ':AIChat<CR>', vim_ai_opts)
+map('x', '<leader>d', ':AIChat ', vim_ai_opts)
+
+-- :AIEdit
+map('n', '<leader>e', ':AIEdit ', vim_ai_opts)
+map('x', '<leader>e', ':AIEdit ', vim_ai_opts)
+
+-- :AIRedo
+map('n', '<leader>r', ':AIRedo<CR>', vim_ai_opts)
+map('x', '<leader>r', ':AIRedo<CR>', vim_ai_opts)
+
+-- :AI
+map('n', '<leader>a', ':AI<CR>', vim_ai_opts)
+map('x', '<leader>a', ':AI<CR>', vim_ai_opts)
