08b4437b8e4b3b8ac437b7ec88f9de945dbd4969
Wojciech Majewski <wojciech.majewski@pm.me>
2023-05-06 03:02:28 +0200

    fish: Add aichat-popup function and update aichat.

--- fish/functions/aichat.fish
+++ fish/functions/aichat.fish
@@ -1,9 +1,9 @@
 function aichat
-  if $1
-    set aichat_path $1
+  if test -n "$1"
+    set aichat_path "$1"
   else
     set aichat_path (mktemp ~/Code/jumski/aichats/tempXXXXXX.aichat)
   end
 
-  vim -c AIChat -c startinsert $aichat_path
+  vim -c AIChat -c startinsert "$aichat_path"
 end
