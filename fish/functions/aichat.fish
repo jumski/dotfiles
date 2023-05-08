2d1685f323773fb5cd54b193a6fbfdf8f43689f0
Wojciech Majewski <wojciech.majewski@pm.me>
2023-05-08 19:54:33 +0200

    fish: Add user input to AIChat prompt in aichat.fish commit.

--- fish/functions/aichat.fish
+++ fish/functions/aichat.fish
@@ -5,12 +5,17 @@ function aichat
     set aichat_path "$1"
   else
     read --prompt-str "Prompt > " prompt
+
+    if test $status -ne 0
+      echo "Exiting AIChat"
+      return
+    end
+
     set slug (slugify "$prompt" | string sub -l 80)
 
     set aichat_path (mktemp "$aichats_dir/"{$slug}"_XXXXXX.aichat")
-
   end
 
-  echo -e ">>> user\n\n" >> "$aichat_path"
+  echo -e ">>> user\n\n$prompt\n\n" >> "$aichat_path"
   vim -c AIChat -c startinsert "$aichat_path"
 end
