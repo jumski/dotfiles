ac4fbedc13af9b827e2694d56f024cddcf1aa675
Wojciech Majewski <wojciech.majewski@pm.me>
2023-05-08 18:52:34 +0200

    fish: Add slugify function and update aichat function to use it for generating temp file names.

--- fish/functions/aichat.fish
+++ fish/functions/aichat.fish
@@ -4,10 +4,13 @@ function aichat
   if test -n "$1"
     set aichat_path "$1"
   else
-    set aichat_path (mktemp "$aichats_dir/tempXXXXXX.aichat")
+    read --prompt-str "Prompt > " prompt
+    set slug (slugify "$prompt" | string sub -l 80)
+
+    set aichat_path (mktemp "$aichats_dir/"{$slug}"_XXXXXX.aichat")
+
   end
 
   echo -e ">>> user\n\n" >> "$aichat_path"
-  read --prompt-str "Prompt > " >> "$aichat_path"
   vim -c AIChat -c startinsert "$aichat_path"
 end
