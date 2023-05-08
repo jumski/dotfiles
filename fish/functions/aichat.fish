c838373fd31bfa36c47c48ff152532115ef2ed0e
Wojciech Majewski <wojciech.majewski@pm.me>
2023-05-08 18:36:17 +0200

    fish: Add aichats_dir variable to aichat function

--- fish/functions/aichat.fish
+++ fish/functions/aichat.fish
@@ -1,8 +1,10 @@
 function aichat
+  set aichats_dir "/home/jumski/ObszaryOdpowiedzialnosci/Komputery i Internet/aichats"
+
   if test -n "$1"
     set aichat_path "$1"
   else
-    set aichat_path (mktemp ~/Code/jumski/aichats/tempXXXXXX.aichat)
+    set aichat_path (mktemp "$aichats_dir/tempXXXXXX.aichat")
   end
 
   echo -e ">>> user\n\n" >> "$aichat_path"
