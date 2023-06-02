07937b354a1b005d9d6f041e2945cdfcae9a17b7
Wojciech Majewski <wojciech.majewski@pm.me>
2023-06-02 15:46:08 +0200

    rails: add fish functions for dc

---
+++ rails/aliases.fish
@@ -0,0 +1,8 @@
+alias prod-heroku='heroku run --app toolchest-production'
+alias stag-heroku='heroku run --app toolchest-staging'
+
+alias rr='docker compose run web bin/rails'
+
+alias devdb-redo='rr db:drop db:create db:environment:set db:schema:load db:migrate db:seed db:migrate:status'
+alias testdb-redo='RAILS_ENV=test rr db:drop db:create db:environment:set db:schema:load db:migrate db:migrate:status'
+alias db-redo='devdb-redo && testdb-redo'
