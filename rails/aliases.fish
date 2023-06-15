b3ed591e323394a3ced1cceb39d14fee5545a07f
Wojciech Majewski <wojciech.majewski@pm.me>
2023-06-15 12:12:16 +0200

    Add dev-redo command to aliases.fish file.

--- rails/aliases.fish
+++ rails/aliases.fish
@@ -6,3 +6,5 @@ alias rr='docker compose run web bin/rails'
 alias devdb-redo='rr db:drop db:create db:environment:set db:schema:load db:migrate db:seed db:migrate:status'
 alias testdb-redo='RAILS_ENV=test rr db:drop db:create db:environment:set db:schema:load db:migrate db:migrate:status'
 alias db-redo='devdb-redo && testdb-redo'
+
+alias dev-redo='docker compose build web && docker compose run web yarn install && docker compose run web bundle install && docker compose build web'
