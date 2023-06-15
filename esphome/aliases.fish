72fc97292b24e8674f8354d36d6f45210dd64f70
Wojciech Majewski <wojciech.majewski@pm.me>
2023-06-15 14:34:37 +0200

    esphome: fix alias so it uses host network and also autocompletes paths

--- esphome/aliases.fish
+++ esphome/aliases.fish
@@ -1,1 +1,4 @@
-alias esphome="docker run --rm --privileged --device=/dev/ttyUSB0 -v .:/config -it ghcr.io/esphome/esphome"
+function esphome --wraps='__fish_complete_path' --description 'alias esphome=docker run --rm --privileged --device=/dev/ttyUSB0 -v .:/config -it ghcr.io/esphome/esphome'
+  docker run --rm --privileged --net=host --device=/dev/ttyUSB0 -v .:/config -it ghcr.io/esphome/esphome $argv
+end
+
