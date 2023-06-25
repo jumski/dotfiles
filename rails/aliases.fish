17c1cf99c60435cc87cf658dcb270ee5ecd43648
Wojciech Majewski <wojciech.majewski@pm.me>
2023-06-25 19:27:39 +0200

    Update rails/aliases.fish for docker-compose usage.

--- rails/aliases.fish
+++ rails/aliases.fish
@@ -6,7 +6,9 @@ function rr --wraps='__fish_complete_path' --description 'docker compose run web
 end
 
 alias devdb-redo='rr db:drop db:create db:environment:set db:schema:load db:migrate db:seed db:migrate:status'
-alias testdb-redo='RAILS_ENV=test rr db:drop db:create db:environment:set db:schema:load db:migrate db:migrate:status'
+alias testdb-redo='docker compose run -e RAILS_ENV=test web bin/rails db:drop db:create db:environment:set db:schema:load db:migrate db:migrate:status'
 alias db-redo='devdb-redo && testdb-redo'
 
 alias dev-redo='docker compose build web && docker compose run web yarn install && docker compose run web bundle install && docker compose build web'
+
+alias full-redo='dev-redo && db-redo'
