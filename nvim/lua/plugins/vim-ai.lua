1fcfbc97938de78f4795daaac6573ce11967b443
Wojciech Majewski <wojciech.majewski@pm.me>
2023-07-03 23:18:38 +0200

    nvim: vim-ai - change the AI model to gpt-4

--- nvim/lua/plugins/vim-ai.lua
+++ nvim/lua/plugins/vim-ai.lua
@@ -9,8 +9,8 @@ You are a concise helpful assistant and expert programmer.
 
 vim.g.vim_ai_chat = {
   options = {
-    model = "gpt-3.5-turbo",
-    -- model = "gpt-4",
+    -- model = "gpt-3.5-turbo",
+    model = "gpt-4",
     temperature = 0.7,
     initial_prompt = initial_chat_prompt
   }
