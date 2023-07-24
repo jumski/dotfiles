df0662f91018fd75051a3ae4c7389895a8e4f642
Wojciech Majewski <wojciech.majewski@pm.me>
2023-07-25 01:28:35 +0200

    add new colorschemes for eink

---
+++ fish/eink_theme.fish
@@ -0,0 +1,13 @@
+# eink color scheme
+set -g color_black '000000'
+set -g color_gray1 '888888'
+set -g color_gray2 '444444'
+set -g color_white 'FFFFFF'
+set -g fish_color_normal $color_black
+set -g fish_color_command $color_gray1
+set -g fish_color_param $color_gray2
+set -g fish_color_error $color_black --underline
+set -g fish_color_autosuggestion $color_gray2 --italic
+set -g fish_color_cwd $color_gray1 --bold
+set -g fish_color_git_dirty $color_gray2 --italic
+
