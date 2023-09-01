658a9bd5e549fb65a988efa0ee6ccbc44b21d23c
Wojciech Majewski <wojciech.majewski@pm.me>
2023-09-01 16:29:32 +0200

    fish: update worldtime with argentina time

--- fish/functions/worldtime.fish
+++ fish/functions/worldtime.fish
@@ -4,8 +4,10 @@ function worldtime
     set -l poland (env TZ=Europe/Warsaw date $format)
     set -l austin (env TZ=America/Chicago date $format)
     set -l manilla (env TZ=Asia/Manila date $format)
+    set -l argentina (env TZ=America/Argentina/Buenos_Aires date $format)
 
-    printf 'Austin:      %s\n' $austin
-    printf 'Phillipines: %s\n' $manilla
-    printf 'Poland:      %s\n' $poland
+    printf 'Austin (USA): %s\n' $austin
+    printf 'Phillipines:  %s\n' $manilla
+    printf 'Argentina:    %s\n' $argentina
+    printf 'Poland:       %s\n' $poland
 end
