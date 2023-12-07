78bc147b8d03f2e165f8b599bbe27f6bca0ea2c5
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-08 00:24:20 +0100

    nvim: disable lspsaga

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -1,10 +1,11 @@
 return {
   'nvimdev/lspsaga.nvim',
+  enabled = false,
   config = function()
     require('lspsaga').setup({})
   end,
   dependencies = {
-    'nvim-treesitter/nvim-treesitter', -- optional
-    -- 'nvim-tree/nvim-web-devicons'
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons'
   }
 }
