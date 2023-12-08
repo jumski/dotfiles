6c53de3b762653d662c69e12f00530e2930c5b43
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-08 01:04:51 +0100

    nvim: disable lazy.nvim change detection notifications

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  import = "plugins",
+  options = {
+    change_detection = {
+      enabled = true,
+      notify = false
+    }
+  }
+})
