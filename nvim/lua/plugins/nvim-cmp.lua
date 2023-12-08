12698856b3ca3ef37d33d83793336d7cde304233
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-08 13:27:43 +0100

    nvim: remove dead code

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -19,14 +19,10 @@ return {
       snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
           require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-          -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-          -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         end,
       },
       window = {
-        -- completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
@@ -36,10 +32,6 @@ return {
         ['<C-e>'] = cmp.mapping.abort(),
         ['<C-CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
       }),
-  -- {name = 'path'},
-  -- {name = 'nvim_lsp', keyword_length = 1},
-  -- {name = 'buffer', keyword_length = 3},
-  -- {name = 'luasnip', keyword_length = 2},
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' }, -- For luasnip users.
