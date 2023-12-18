7646568de191adce0074206e0b7d59aaea3d916b
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-18 18:09:40 +0100

    nvim: disable custom vim-dadbod-ui mapping

--- nvim/lua/plugins/vim-dadbod-ui.lua
+++ nvim/lua/plugins/vim-dadbod-ui.lua
@@ -14,8 +14,7 @@ return {
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_execute_on_save = 1
     vim.g.db_ui_disable_mappings = 1
-
-    vim.keymap.set('n', '<CR>', '<Plug>(DBUI_SelectLine)', { noremap = true, silent = true })
+    -- vim.keymap.set('n', '<CR>', '<Plug>(DBUI_SelectLine)', { noremap = true, silent = true })
 
     -- Lua autocmd for FileType dbout to setlocal nofoldenable
     vim.api.nvim_create_autocmd("FileType", {
