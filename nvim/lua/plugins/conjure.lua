35f616e1643858b38081ad941259f849f1f8cfa2
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-18 23:41:54 +0100

    nvim: remap conjure from K to gk

--- nvim/lua/plugins/conjure.lua
+++ nvim/lua/plugins/conjure.lua
@@ -27,5 +27,14 @@ return {
     init = function()
 	       -- Set configuration options here
         vim.g["conjure#debug"] = true
+
+        -- Disable the documentation mapping
+        -- vim.g["conjure#mapping#doc_word"] = false
+
+        -- Rebind it from K to <prefix>gk
+        vim.g["conjure#mapping#doc_word"] = "gk"
+
+        -- Reset it to the default unprefixed K (note the special table wrapped syntax)
+        -- vim.g["conjure#mapping#doc_word"] = {"K"}
     end,
 }
