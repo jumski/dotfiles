a397dd4b63f460c827f15388abd37e9bc8109087
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-20 21:22:17 +0100

    qmk: improve install.sh

--- qmk/install.sh
+++ qmk/install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# if there is no /home/jumski/qmk_firmware directory, then clone it
+if [ ! -f "/home/jumski/qmk_firmware/util/udev/50-qmk.rules" ]; then
+  git clone https://github.com/qmk/qmk_firmware.git /home/jumski/qmk_firmware
+fi
+
 sudo cp /home/jumski/qmk_firmware/util/udev/50-qmk.rules /etc/udev/rules.d/
