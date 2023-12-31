e4ef8249a08719fb17a7da5852f03f9c8d4cd092
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-31 17:40:15 +0100

    vim: remove yarn pkg from vim

--- vim/install.sh
+++ vim/install.sh
@@ -4,7 +4,3 @@ if ! test -f $HOME/.local/share/nvim/site/autoload/plug.vim; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
-
-if which yarn 2>&1 >/dev/null; then
-  yarn global add instant-markdown-d
-fi
