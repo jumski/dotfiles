b5f1089292f240a2764cb2a5f13ff0323addbf9f
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-31 13:02:56 +0100

    nvim: add mason

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,33 @@
+return {
+  'williamboman/mason.nvim',
+  dependencies = {
+    'williamboman/mason-lspconfig.nvim',
+		-- "WhoIsSethDaniel/mason-tool-installer.nvim"
+  },
+  config = function()
+    local mason = require('mason')
+    local mason_lspconfig = require('mason-lspconfig')
+    -- local mason_tool_installer = require("mason-tool-installer")
+
+    mason.setup({})
+    mason_lspconfig.setup({
+      ensure_installed = {
+        'solargraph',
+        'sorbet',
+        'lua_ls',
+        'pyright',
+        'pyright',
+        'tsserver',
+        'cssmodules_ls',
+        'cssls',
+        'sqlls',
+        'clojure_lsp',
+        'svelte'
+      },
+      automatic_installation = true
+    })
+
+--     mason_tool_installer.setup({
+--     })
+  end
+}
