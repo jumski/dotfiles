69bac28a7448937fc28deed6e9e3305ab1ece274
Wojciech Majewski <wojciech.majewski@pm.me>
2023-12-31 11:43:20 +0100

    nvim: update treesitter build opt, add fish lang

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,13 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup {
       ensure_installed = {
         "c",
         "clojure",
         "css",
+        "fish",
         "javascript",
         "lua",
         "python",
