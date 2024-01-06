db5b58170d06f8f08420ccb87c068ba5187f94bd
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-06 16:46:28 +0100

    nvim: do not require lspcontainers

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -6,7 +6,6 @@ return {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
-    'lspcontainers/lspcontainers.nvim',
     'kristijanhusak/vim-dadbod-completion',
   },
   config = function()
