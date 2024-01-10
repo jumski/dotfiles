a88e4083d7a3e588d8c9517f73398490574552a9
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-10 18:11:02 +0100

    nvim: disable autopairing of `

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,5 +1,11 @@
 return {
     'windwp/nvim-autopairs',
+    -- enabled = false,
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+    opts = { }, -- this is equalent to setup({}) function
+    config = function(_, opts)
+      local npairs = require('nvim-autopairs')
+      npairs.setup(opts)
+      npairs.remove_rule("`")
+    end
 }
