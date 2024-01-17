a30509ab456b8c78eee0e0bcf20195241508a310
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-17 09:30:17 +0100

    fonts: remove it altogether because of commit-mono

--- fonts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-destination_path=/usr/share/fonts/truetype/ttf-monaco/
-
-if [ ! -f $destination_path ]; then
-  sudo mkdir -p $destination_path
-  sudo cp fonts/*.{ttf,otf} $destination_path &&
-    sudo fc-cache -fv
-fi
