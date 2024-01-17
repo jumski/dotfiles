d95ea61e246a7ab349a2a2392cda00abd4fd20bf
Wojciech Majewski <wojciech.majewski@pm.me>
2024-01-17 16:11:58 +0100

    direnv: speed up install.sh

--- direnv/install.sh
+++ direnv/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if command -v asdf &> /dev/null
-then
+if asdf plugin list | grep -q 'direnv'; then
+  echo "direnv plugin is already installed."
+else
   asdf plugin add direnv
   asdf direnv setup --shell fish --version system
-else
-  echo "asdf is not installed. Please install it first."
 fi
