b414a7b47b1631c2c02bbab91e1d22c9abe219b9
Wojciech Majewski <wojciech.majewski@pm.me>
2024-02-20 11:50:51 +0100

    setup bitwarden-cli for neovim's ChatGPT plugin (WIP)

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -1,9 +1,13 @@
 return {
   "jackMort/ChatGPT.nvim",
-  -- enabled = false,
+  enabled = false,
   event = "VeryLazy",
-  config = function()
-    require("chatgpt").setup()
+  config = function(_, opts)
+    local home = vim.fn.expand("$HOME")
+
+    require("chatgpt").setup({
+      api_key_cmd = home .. "/.dotfiles/bin/get_openai_token",
+    })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
