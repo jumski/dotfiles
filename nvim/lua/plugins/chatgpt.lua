a167b1b06f803e5570eba3c6e5e9556f4837c71b
Wojciech Majewski <wojciech.majewski@pm.me>
2024-02-20 19:53:17 +0100

    nvim: working chatgpt config

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -1,12 +1,13 @@
 return {
   "jackMort/ChatGPT.nvim",
-  enabled = false,
+  -- enabled = false,
   event = "VeryLazy",
   config = function(_, opts)
     local home = vim.fn.expand("$HOME")
 
     require("chatgpt").setup({
-      api_key_cmd = home .. "/.dotfiles/bin/get_openai_token",
+      api_key_cmd = home .. "/.get_openai_token",
+      -- api_key_cmd = home .. "/.dotfiles/bin/get_openai_token",
     })
   end,
   dependencies = {
