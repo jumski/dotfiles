3647d2999fa51d433529b9b997121c90bb98aab3
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-09 13:11:12 +0100

    Add lefthook, now only checks lua

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -1,3 +1,28 @@
+local CONFIG_TYPE = 'ollama' -- or 'openai'
+
+local ollama_config = {
+  api_host_cmd = 'echo -n http://jumski-manjaro-pc.local:11434',
+  api_key_cmd = 'echo whatever',
+  openai_params = {
+    model = "dolphin-mixtral",
+    frequency_penalty = 0,
+    presence_penalty = 0,
+    max_tokens = 300,
+    temperature = 0,
+    top_p = 1,
+    n = 1,
+  },
+  openai_edit_params = {
+    model = "dolphin-mixtral",
+    frequency_penalty = 0,
+    presence_penalty = 0,
+    temperature = 0,
+    top_p = 1,
+    n = 1,
+  },
+}
+
+
 return {
   "jackMort/ChatGPT.nvim",
   -- enabled = false,
@@ -5,10 +30,17 @@ return {
   config = function(_, opts)
     local home = vim.fn.expand("$HOME")
 
-    require("chatgpt").setup({
+    local openai_config = {
       api_key_cmd = home .. "/.get_openai_token",
-      -- api_key_cmd = home .. "/.dotfiles/bin/get_openai_token",
-    })
+    }
+
+    local config
+
+    if CONFIG_TYPE == 'openai' then
+      require("chatgpt").setup(openai_config)
+    else
+      require("chatgpt").setup(ollama_config)
+    end
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
