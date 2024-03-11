1ed792c4ec7eea84a7162d13eeaaca8d68b8ed46
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-11 11:49:46 +0100

    fish: speed up and fix install.sh

--- fish/install.sh
+++ fish/install.sh
@@ -4,10 +4,16 @@ if !which fish &>/dev/null; then
   chsh -s $(which fish)
 fi
 
-if which fisher &>/dev/null; then
-  echo Fisher installed, skipping
-else
+if ! [ -f ~/.config/fish/functions/fisher.fish ]; then
   echo Installing fisher
   curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+else
+  echo Fisher installed, skipping
 fi
 
+# set fish shell for user jumski
+if [ $(getent passwd jumski | cut -d: -f7) != /usr/bin/fish ]; then
+  chsh -s /usr/bin/fish jumski
+else
+  echo "User 'jumski' already has a 'fish' shell."
+fi
