5c8747db1864b5ca48f31895bbf398f1b16b1b7f
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-11 20:11:16 +0100

    nvim: setup which-key mappings for chatgpt

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -1,6 +1,27 @@
-local CONFIG_TYPE = 'ollama' -- or 'openai'
+local CHAT_API =
+  'ollama';
+  -- 'openai';
 
-local keymap = {
+local WHICH_KEY_MAPPINGS = {
+  c = {
+    name = "ChatGPT",
+    c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+    e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+    g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+    t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+    k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+    d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+    a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+    o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+    s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+    f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+    x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+    r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+    l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+  },
+}
+
+local UI_MAPPINGS = {
   close = "<C-c>",
   yank_last = "<C-y>",
   yank_last_code = "<C-k>",
@@ -25,6 +46,12 @@ local keymap = {
   stop_generating = "<C-x>",
 }
 
+local HOME_PATH = vim.fn.expand("$HOME")
+local openai_config = {
+  api_key_cmd = HOME_PATH .. "/.get_openai_token",
+  keymap = UI_MAPPINGS
+}
+
 local ollama_config = {
   api_host_cmd = 'echo -n http://pc.netbird.cloud:11434',
   api_key_cmd = 'echo whatever',
@@ -45,26 +72,7 @@ local ollama_config = {
     top_p = 1,
     n = 1,
   },
-  keymap = keymap
-}
-
-local which_key_mappings = {
-  c = {
-    name = "ChatGPT",
-    c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-    e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-    g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-    t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-    k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-    d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-    a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-    o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-    s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-    f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-    x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-    r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-    l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
-  },
+  keymap = UI_MAPPINGS
 }
 
 return {
@@ -72,25 +80,13 @@ return {
   -- enabled = false,
   event = "VeryLazy",
   config = function()
-    local home = vim.fn.expand("$HOME")
-    local openai_config = {
-      api_key_cmd = home .. "/.get_openai_token",
-      keymap = keymap
-    }
-
-    local map = vim.api.nvim_set_keymap
-    local vim_ai_opts = {noremap = true}
-    map('n', '<leader>d', ':ChatGPT<CR>', vim_ai_opts)
-    map('v', '<leader>d', ':ChatGPT<CR>', vim_ai_opts)
-    -- map('x', '<leader>d', ':AIChat ', vim_ai_opts)
-
-    if CONFIG_TYPE == 'openai' then
+    if CHAT_API == 'openai' then
       require("chatgpt").setup(openai_config)
     else
       require("chatgpt").setup(ollama_config)
     end
 
-    require("which-key").register(which_key_mappings, { prefix = "<leader>", })
+    require("which-key").register(WHICH_KEY_MAPPINGS, { prefix = "<leader>", })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
