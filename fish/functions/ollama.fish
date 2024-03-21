5ee1b86d7a05d9fc91e4f72855d98255f50689fa
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-21 05:54:16 +0100

    fish: update ollama fn

--- fish/functions/ollama.fish
+++ fish/functions/ollama.fish
@@ -3,10 +3,10 @@ function ollama
   set given_value 'jumski-manjaro-pc'
 
   if test (hostname) = $given_value
-    echo run locally
-    # docker compose --file ~/Code/jumski/devmachine-stacks/ollama/compose.yaml exec ollama ollama $argv
+    echo Run Ollama locally
+    docker compose --file ~/Code/jumski/devmachine-stacks/ollama/compose.yaml exec ollama ollama $argv
   else
-    echo run via ssh
-    # ssh pc docker compose --file ~/Code/jumski/devmachine-stacks/ollama/compose.yaml exec ollama ollama $argv
+    echo Run Ollama via SSH to $given_value
+    ssh pc docker compose --file ~/Code/jumski/devmachine-stacks/ollama/compose.yaml exec ollama ollama $argv
   end
 end
