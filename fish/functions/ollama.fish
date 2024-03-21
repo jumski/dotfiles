28aaf9a702ab61bfa0c7ef377210cdf37c0afb27
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-21 05:52:20 +0100

    fish: update ollama function to work via ssh

--- fish/functions/ollama.fish
+++ fish/functions/ollama.fish
@@ -1,4 +1,12 @@
 
 function ollama
-  docker compose --file $HOME/Code/jumski/devmachine-stacks/ollama/compose.yaml exec ollama ollama $argv
+  set given_value 'jumski-manjaro-pc'
+
+  if test (hostname) = $given_value
+    echo run locally
+    # docker compose --file ~/Code/jumski/devmachine-stacks/ollama/compose.yaml exec ollama ollama $argv
+  else
+    echo run via ssh
+    # ssh pc docker compose --file ~/Code/jumski/devmachine-stacks/ollama/compose.yaml exec ollama ollama $argv
+  end
 end
