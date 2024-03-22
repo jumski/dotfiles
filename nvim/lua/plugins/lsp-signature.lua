9b2d7402892e3518acc1816156486fc7abc56de0
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-22 11:55:40 +0100

    nvim: re-enable lsp_signature

--- nvim/lua/plugins/lsp-signature.lua
+++ nvim/lua/plugins/lsp-signature.lua
@@ -1,7 +1,6 @@
 return {
   "ray-x/lsp_signature.nvim",
-  enabled = false,
+  -- enabled = false,
   event = "VeryLazy",
-  opts = {},
-  config = function(_, opts) require'lsp_signature'.setup(opts) end
+  config = true
 }
