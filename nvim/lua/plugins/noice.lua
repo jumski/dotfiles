4d05abc7855e1298c76748aec08dcb87afca5aa6
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-22 11:45:09 +0100

    nvim: updates to noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -13,10 +13,26 @@ return {
     -- you can enable a preset for easier configuration
     presets = {
       bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = false, -- position the cmdline and popupmenu together
+      command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
       inc_rename = false, -- enables an input dialog for inc-rename.nvim
       lsp_doc_border = false, -- add a border to hover docs and signature help
+    },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = '50%',
+          col = '50%',
+        },
+        border = {
+          style = "none",
+          padding = { 2, 3 },
+        },
+        filter_options = {},
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
     },
     -- add any options here
   },
