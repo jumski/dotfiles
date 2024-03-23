92c05677c0ec6ba3d61e54f3b5335df30e218d81
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-23 01:03:08 +0100

    nvim: prettify which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -3,7 +3,14 @@ return {
   event = "VeryLazy",
   opts = {
     window = {
-      border = "single"
+      border = "none",
+      position = "bottom",
+      margin = { 0, 0, 0, 0 },
+      padding = { 0, 0, 0, 0 },
+    },
+    layout = {
+      align="center",
+      spacing = 3
     }
   }
 }
