b868a6875d85c850ebc013500d14c37c88d8fa51
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-26 02:19:51 +0100

    nvim: change lualine theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require('lualine').setup {
       options = {
-        theme = 'gruvbox',
+        theme = 'tokyonight_storm',
         icons_enabled = true,
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''},
