b0d5c534f3abfcedea1eba9901270611b95e0279
Wojciech Majewski <wojciech.majewski@pm.me>
2024-03-28 17:26:08 +0100

    bat: change from env to config file, add some syntax WIP
    not all works yet - some problem with theme

--- bat/env.fish
+++
@@ -1,4 +0,0 @@
-set -gx BAT_THEME tokyonight_storm
-set -gx BAT_STYLE header-filename,grid
-set -gx BAT_PAGING auto
-set -gx BAT_TABS 2
--- bat/install.sh
+++ bat/install.sh
@@ -1,8 +1,8 @@
-# install theme
-# TODO: take care of me!!!
-#if ! bat --list-themes | grep Gruvbox-N 2>&1 >/dev/null; then
-#  mkdir -p "$(bat cache --config-dir)/themes"
-#  cd "$(bat cache --config-dir)/themes"
-#  git clone https://github.com/jumski/Colorsublime-Themes
-#  bat cache --init
-#fi
+#!/bin/bash
+
+set -e
+
+if ! bat --list-themes | grep tokyonight 2>&1 >/dev/null; then
+  echo Caching bat themes
+  bat cache --build
+fi
