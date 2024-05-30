7ce425dc149e97b3e4d79e41ba6b37b7ba570354
Wojtek Majewski <wojciech.majewski@pm.me>
2024-05-30 13:33:34 +0200

    system: remove ./bin from PATH

--- system/_path.fish
+++ system/_path.fish
@@ -1,2 +1,2 @@
-set -x PATH "./bin:/usr/local/bin:/usr/local/sbin:$HOME/.dotfiles/bin:$PATH"
+set -x PATH "/usr/local/bin:/usr/local/sbin:$HOME/.dotfiles/bin:$PATH"
 set -x MANPATH "/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
