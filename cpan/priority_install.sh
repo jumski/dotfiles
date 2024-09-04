0787a9059da111d30fc65d2587ee46913430389d
Wojtek Majewski <wojciech.majewski@pm.me>
2024-09-04 08:46:38 +0200

    cpan: use abs path to bin

--- cpan/priority_install.sh
+++ cpan/priority_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+cpan_bin=/usr/bin/core_perl/cpan
+
 # make sure `git add -p` can be accepted with single keypress
 if perl -MTerm::ReadKey -e '' 2>/dev/null; then
   echo "Term::ReadKey is already installed."
 else
   echo "Term::ReadKey is not installed, installing now..."
-  sudo PERL_MM_USE_DEFAULT=1 PERL_AUTOINSTALL=--alldeps cpan -T Term::ReadKey
+  sudo PERL_MM_USE_DEFAULT=1 PERL_AUTOINSTALL=--alldeps $cpan_bin -T Term::ReadKey
 fi
