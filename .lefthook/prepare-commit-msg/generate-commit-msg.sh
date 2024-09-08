c08d83460ff6694dab4d51297a030beb7b55e04f
Wojtek Majewski <wojciech.majewski@pm.me>
2024-09-08 10:25:51 +0200

    feat: add automated commit message generation
    - Introduced `generate-commit-msg.sh` script to automatically generate commit messages using gpt
    - Configured Lefthook to use the new script in the `prepare-commit-msg` hook.
    - Updated `lefthook.yml` to include the new `prepare-commit-msg` hook and removed unused confs

---
+++ .lefthook/prepare-commit-msg/generate-commit-msg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+COMMIT_MSG_FILE="$1"
+COMMIT_SOURCE="$2"
+
+openai_model=gpt-4o
+# openai_model=gpt-4o-mini
+
+function generate_with() {
+  echo $diff | sgpt --no-cache --model $openai_model --role commit-msg | sed 's/^\s*```//;s/```\s*$//' | awk 'NF {p=1} p; {if (NF) {p=1}}'
+}
+
+if [ -z "$COMMIT_SOURCE" ]; then
+    diff=$(git diff --cached)
+    diff_length=${#diff}
+
+    if [ $diff_length -lt 200000 ]; then # ~50k input tokens, 0.0075$
+        generate_with $openai_model > "$COMMIT_MSG_FILE"
+    else
+        echo "<<< DIFF TOO LONG - message generation skipped >>>" > "$COMMIT_MSG_FILE"
+    fi
+fi
