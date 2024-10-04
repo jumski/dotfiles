2ebe501aa20c1a9fab0153ba1c0b2c3e778e5391
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-04 09:47:26 +0200

    fix(cmp): disable luasnip source in completion sources configuration

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -33,7 +33,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
         { name = 'nvim_lsp_signature_help' },
       }, {
         { name = 'buffer' },
