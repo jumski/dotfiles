9e948d774a556ca564bdb1497f77374afb6c4237
Wojtek Majewski <wojciech.majewski@pm.me>
2024-10-29 13:50:14 +0100

    feat(vim-dadbod-ui): enable nvim notify for notifications
    Added configuration to use `folke/noice` for notifications by setting `vim.g.db_ui_use_nvim_notify`
    to 1. This enhances the notification system within the vim-dadbod-ui plugin.

--- nvim/lua/plugins/vim-dadbod-ui.lua
+++ nvim/lua/plugins/vim-dadbod-ui.lua
@@ -33,6 +33,11 @@ return {
 
     vim.g.db_ui_use_nerd_fonts = 1
 
+    -- use folke/noice for notifications
+    vim.g.db_ui_use_nvim_notify = 1
+    -- alternative, untested:
+    -- g:db_ui_force_echo_notifications
+
     -- be explicit about execution with which-key mappings
     vim.g.db_ui_execute_on_save = 0
 
