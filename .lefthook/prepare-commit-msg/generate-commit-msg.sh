b4a12bc3efca70f337d5037494505fc31deba8f9
Wojtek Majewski <wojciech.majewski@pm.me>
2024-11-01 11:46:38 +0100

    fix(prepare-commit-msg): adjust diff length handling for commit message generation
    - Removed unnecessary diff length check and simplified the logic for generating commit messages.
    - Ensured the diff is captured up to 5000 characters for processing.
    - Streamlined the script by removing redundant variable assignments.

--- .lefthook/prepare-commit-msg/generate-commit-msg.sh
+++ .lefthook/prepare-commit-msg/generate-commit-msg.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-COMMIT_MSG_FILE="$1"
-COMMIT_SOURCE="$2"
-
 openai_model=gpt-4o
 # openai_model=gpt-4o-mini
+
+COMMIT_MSG_FILE="$1"
+COMMIT_SOURCE="$2"
 
 function generate_with() {
   echo $diff | sgpt --no-cache --model $openai_model --role commit-msg | sed 's/^\s*```//;s/```\s*$//' | awk 'NF {p=1} p; {if (NF) {p=1}}'
 }
 
 if [ -z "$COMMIT_SOURCE" ]; then
-    diff=$(git diff --cached -- . ':(exclude)pnpm-lock.yaml' | head -c 4999)
-    diff_length=${#diff}
+    diff=$(git diff --cached -- . ':(exclude)pnpm-lock.yaml' | head -c 5000)
 
-    if [ $diff_length -lt 5000 ]; then
-        generate_with $openai_model | fold -w 100 > "$COMMIT_MSG_FILE"
-    else
-        echo "<<< DIFF TOO LONG - message generation skipped >>>" > "$COMMIT_MSG_FILE"
-    fi
+    generate_with $openai_model | fold -w 100 > "$COMMIT_MSG_FILE"
 fi
