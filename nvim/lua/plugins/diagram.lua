e386efcb737607b85153b9b34b34eab0d4f38bb9
Wojtek Majewski <wojciech.majewski@pm.me>
2024-12-02 14:12:24 +0100

    feat(diagram): add configuration and dependencies for diagram.nvim
    - Added `hererocks` plugin to `lazy-lock.json` with commit `c9c5444dea1e07e005484014a8231aa667be30b6
    `.
    - Updated `diagram.lua` to include `image.nvim` as a dependency.
    - Configured `diagram.nvim` with markdown integration and set mermaid renderer theme to "forest".

--- nvim/lua/plugins/diagram.lua
+++ nvim/lua/plugins/diagram.lua
@@ -1,4 +1,18 @@
 return {
   "3rd/diagram.nvim",
-  config = true,
+  dependencies = {
+    "3rd/image.nvim",
+  },
+  config = function()
+    require("diagram").setup({
+      integrations = {
+        require("diagram.integrations.markdown"),
+      },
+      renderer_options = {
+        mermaid = {
+          theme = "forest",
+        },
+      },
+    })
+  end,
 }
