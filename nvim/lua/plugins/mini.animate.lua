8d52ec3f9ac8290212f062b8ee5b0b2a5b590dad
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-03 02:13:21 +0100

    feat(mini.animate): add config to disable animations for Avante chat input and window
    - Set up `mini.animate` with a configuration function.
    - Add an autocommand to disable `mini.animate` and `mini.indentscope` for file types "Avante" and "A
    vanteInput".

--- nvim/lua/plugins/mini.animate.lua
+++ nvim/lua/plugins/mini.animate.lua
@@ -2,4 +2,15 @@ return {
   "echasnovski/mini.animate",
   version = "*",
   opts = {},
+  config = function()
+    require("mini.animate").setup()
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "Avante", "AvanteInput" },
+      callback = function()
+        vim.b.minianimate_disable = true     -- For mini.animate
+        vim.b.miniindentscope_disable = true -- For mini.indentscope
+      end,
+    })
+  end,
 }
