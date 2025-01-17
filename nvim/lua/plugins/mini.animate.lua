2272bc3e347741211e215249f006e80637e4ea1f
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-17 15:53:09 +0100

    perf(animation): adjust mini.animate timing parameters
    reduce cursor, scroll, and resize animation timings for smoother performance

--- nvim/lua/plugins/mini.animate.lua
+++ nvim/lua/plugins/mini.animate.lua
@@ -1,14 +1,15 @@
 return {
   "echasnovski/mini.animate",
+  -- enabled = false,
   version = "*",
   opts = {},
   config = function()
     require("mini.animate").setup({
       cursor = {
-        timing = function() return 11 end, -- Default is 110
+        timing = function() return 9 end, -- Default is 110
       },
       scroll = {
-        timing = function() return 11 end, -- Default is 110
+        timing = function() return 5 end, -- Default is 110
       },
       resize = {
         timing = function() return 11 end, -- Default is 110
