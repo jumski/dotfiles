45d12caf14e6dde551a2684de41bd846f42e6b08
Wojtek Majewski <wojciech.majewski@pm.me>
2025-01-31 09:36:17 +0100

    feat(avante): configure claude and ollama providers with updated settings
    add ollama vendor configuration with custom endpoint and model
    update claude provider to use specific model
    adjust plugin configuration and dependencies

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,7 +1,7 @@
 return {
   -- enable Avante only if the ANTHROPIC_API_KEY env variable is set
   -- this is because Avante nags with api key dialog if it is not set
-  enabled = (vim.fn.getenv("ANTHROPIC_API_KEY") ~= vim.NIL);
+  enabled = (vim.fn.getenv("ANTHROPIC_API_KEY") ~= vim.NIL),
 
   "yetone/avante.nvim",
   lazy = false,
@@ -12,8 +12,41 @@ return {
     -- provider = "openai"
     provider = "claude",
     claude = {
-      model = "claude-3-5-sonnet-20241022"
-    }
+      model = "claude-3-5-sonnet-20241022",
+    },
+    vendors = {
+      ollama = {
+        __inherited_from = "openai",
+        api_key_name = "",
+        endpoint = "http://pc.netbird.cloud:11434/v1",
+        model = "deepseek-r1:32b",
+        -- temperature = 0,
+        -- max_tokens = 8192,
+      },
+    },
+    -- ollama = {
+    --   ["local"] = true,
+    --   -- api_key_name = "",
+    --   parse_curl_args = function(opts, code_opts)
+    --     return {
+    --       url = opts.endpoint .. "/chat/completions",
+    --       headers = {
+    --         ["Accept"] = "application/json",
+    --         ["Content-Type"] = "application/json",
+    --         ["x-api-key"] = "ollama",
+    --       },
+    --       body = {
+    --         model = opts.model,
+    --         messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+    --         max_tokens = 2048,
+    --         stream = true,
+    --       },
+    --     }
+    --   end,
+    --   parse_response_data = function(data_stream, event_state, opts)
+    --     require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+    --   end,
+    -- },
     -- add any opts here
   },
   dependencies = {
@@ -23,7 +56,7 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below is optional, make sure to setup it properly if you have lazy=true
     {
-      'MeanderingProgrammer/render-markdown.nvim',
+      "MeanderingProgrammer/render-markdown.nvim",
       opts = {
         file_types = { "markdown", "Avante" },
       },
