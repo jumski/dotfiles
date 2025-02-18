169b48a20ba8b8e4738bdd374aa05b00cb780d01
Wojtek Majewski <wojciech.majewski@pm.me>
2025-02-18 11:34:03 +0100

    style(nvim): clean up whitespace and formatting in settings
    minor formatting adjustments to improve code consistency without changing behavior

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -1,27 +1,27 @@
-local cmd = vim.cmd           -- execute Vim commands
+local cmd = vim.cmd -- execute Vim commands
 local exec = vim.api.vim_exec -- execute Vimscript
-local fn = vim.fn             -- call Vim functions
-local g = vim.g               -- global variables
-local opt = vim.opt           -- global/buffer/windows-scoped options
+local fn = vim.fn -- call Vim functions
+local g = vim.g -- global variables
+local opt = vim.opt -- global/buffer/windows-scoped options
 
-opt.shell = "/bin/sh"         -- vim must use POSIX shell and fish is not POSIX
+opt.shell = "/bin/sh" -- vim must use POSIX shell and fish is not POSIX
 
 --- General
-opt.mouse = "a"                                                -- enable mouse support
-opt.swapfile = false                                           -- do not use swapfile
-opt.writebackup = false                                        -- do not use backup files
-opt.history = 5000                                             -- remember n-lines in history
-opt.modeline = false                                           -- disable reading modelines
-opt.hidden = true                                              -- 1. The current buffer can be put to the background without writing to disk
+opt.mouse = "a" -- enable mouse support
+opt.swapfile = false -- do not use swapfile
+opt.writebackup = false -- do not use backup files
+opt.history = 5000 -- remember n-lines in history
+opt.modeline = false -- disable reading modelines
+opt.hidden = true -- 1. The current buffer can be put to the background without writing to disk
 -- 2. When a background buffer becomes current again, marks and undo-history are remembered
 opt.sessionoptions = "buffers,winsize,tabpages,winpos,winsize" -- session saving options
 
-opt.clipboard = "unnamedplus"                                  -- use system clipboard
+opt.clipboard = "unnamedplus" -- use system clipboard
 
 --- Performance
-opt.ttyfast = true    -- indicates a fast terminal connection
+opt.ttyfast = true -- indicates a fast terminal connection
 -- (more characters will be sent to the screen for redrawing)
-opt.timeoutlen = 300  -- time out on mapping
+opt.timeoutlen = 300 -- time out on mapping
 opt.ttimeoutlen = 100 -- time out on key codes after a tenth of a second
 
 -- disabled because noice.nvim complained about it
@@ -34,8 +34,8 @@ opt.termguicolors = true -- needed to properly show colors in tmux
 opt.background = "dark" -- use dark background
 
 --- UI
-opt.number = false  -- do not show line numbering
-opt.laststatus = 2  -- always show status line
+opt.number = false -- do not show line numbering
+opt.laststatus = 2 -- always show status line
 opt.autoread = true -- automatically read file if it changes
 
 -- ignore "Hit enter" messages
@@ -45,35 +45,34 @@ opt.autoread = true -- automatically read file if it changes
 opt.shortmess:append("IWs")
 
 -- (this does not happen if file is deleted)
-opt.showcmd = true    -- show incomplete commands
-opt.showmode = false  -- do not display the mode you're in, because of status line
+opt.showcmd = true -- show incomplete commands
+opt.showmode = false -- do not display the mode you're in, because of status line
 opt.cmdwinheight = 10 -- command history window height
 opt.splitbelow = true -- vertical splits below
 opt.splitright = true -- horizontal splits on right
-opt.incsearch = true  -- show results during typing the search
-opt.scrolloff = 3     -- always show at least n-lines below and above cursor
+opt.incsearch = true -- show results during typing the search
+opt.scrolloff = 3 -- always show at least n-lines below and above cursor
 opt.sidescrolloff = 5 -- always show at least n-chars before and after cursor
-opt.wildmenu = true   -- change behaviour of <TAB> completion of commands
+opt.wildmenu = true -- change behaviour of <TAB> completion of commands
 -- to similar to bash completion
 opt.wildmode = "list:longest,list:full"
 opt.colorcolumn = "80" -- back to the 80s XD
 
 --- Typing / characters / matching
 opt.wildignorecase = true -- ignore case when <TAB>completing filenames
-opt.showmatch = true      -- show matching bracket
-opt.matchtime = 2         -- show it for 2 seconds
-opt.ignorecase = true     -- ignore capitals when searching
-opt.smartcase = true      -- case sensitive search only when first letter is capital
+opt.showmatch = true -- show matching bracket
+opt.matchtime = 2 -- show it for 2 seconds
+opt.ignorecase = true -- ignore capitals when searching
+opt.smartcase = true -- case sensitive search only when first letter is capital
 
 opt.tabstop = 2
 -- "================ TABS AND SPACES
--- set expandtab     " all tabs expands to spaces
--- set sw=2          " automagic indent width
--- set tabstop=2     " size of tab in spaces
--- set ts=2          " size of tab
--- set shiftround    " round indent to multiple of 'shiftwidth', applies to > and <
--- set smarttab
--- set softtabstop=2 " number of spaces that a <Tab> counts for
+opt.expandtab = true -- all tabs expands to spaces
+opt.shiftwidth = 2 -- automagic indent width
+opt.tabstop = 2 -- size of tab in spaces
+opt.shiftround = true -- round indent to multiple of 'shiftwidth', applies to > and <
+opt.smarttab = true -- use shiftwidth for <Tab> and <BS>
+opt.softtabstop = 2 -- number of spaces that a <Tab> counts for
 --                   " while performing editing operations
 
 --- Programs
