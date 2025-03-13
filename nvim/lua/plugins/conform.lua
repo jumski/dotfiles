2c95f0dac460699cbf36cb4317697cebbd388620
Wojtek Majewski <wojciech.majewski@pm.me>
2025-03-13 13:49:34 +0100

    refactor(formatter): improve conform.lua for sqruff
    optimize sqruff formatter setup with dynamic config resolution
    reduce format-on-save timeout and add whitespace trimming options

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -25,32 +25,52 @@ return {
       javascript = { "prettierd", stop_after_first = true },
       typescript = { "prettierd", stop_after_first = true },
       svelte = { "prettierd", stop_after_first = true },
+      -- Add trim_whitespace to all file types
+      -- ["*"] = { "trim_whitespace", "trim_newlines" },
     },
     -- Set default options
     default_format_opts = {
       lsp_format = "fallback",
     },
     -- Set up format-on-save
-    format_after_save = { timeout_ms = 2000 },
+    format_after_save = { timeout_ms = 500 },
     -- Customize formatters
     formatters = {
       shfmt = {
         prepend_args = { "-i", "2" },
       },
-      sqruff = function(_)
-        return {
-          require_cwd = true,
-          stdin = true,
-          args = { "fix", "--parsing-errors", "--force", "-" },
-          cwd = require("conform.util").root_file({
-            ".sqruff",
-            "nx.json",
-            ".editorconfig",
-            ".git",
-            "pnpm-lock.yaml",
-          }),
-        }
-      end,
+      sqruff = {
+        -- Use a function that will be called when the formatter is used
+        cwd = function(self, ctx)
+          -- Load the util module when the function is called
+          return require("conform.util").root_file(".sqruff")(self, ctx)
+        end,
+        args = function(ctx)
+          -- Make sure we have a valid source directory
+          local source_dir = ctx.dirname or vim.fn.expand("%:p:h")
+
+          -- Find the config file
+          local root_dir = vim.fs.root(source_dir, ".sqruff")
+
+          -- Start with base command
+          local args = {}
+
+          -- Add config if found (directly after command name)
+          if root_dir then
+            local config_path = root_dir .. "/.sqruff"
+            table.insert(args, "--config=" .. config_path)
+          end
+
+          -- Add the rest of the arguments
+          table.insert(args, "fix")
+          table.insert(args, "--parsing-errors")
+          table.insert(args, "--force")
+          table.insert(args, "-")
+
+          return args
+        end,
+        stdin = true,
+      },
     },
     log_level = vim.log.levels.DEBUG,
   },
