ecffd80131e8304018ab56086965cb9ee543e4f8
Wojtek Majewski <wojciech.majewski@pm.me>
2025-03-13 13:50:25 +0100

    fix(linter): update sqruff configuration for better lint parsing
    change sqruff diagnostic extra arguments to use lint mode with parsing error detection

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -15,7 +15,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.diagnostics.sqruff.with({
-          extra_args = { "--dialect", "postgres" },
+          extra_args = { "lint", "--parsing-errors" },
         }),
         null_ls.builtins.formatting.prettier.with({
           extra_filetypes = { "astro" },
