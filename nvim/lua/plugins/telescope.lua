c90d19adacb4f4891a45fb4908d443d6efaf66df
Wojtek Majewski <wojciech.majewski@pm.me>
2025-03-31 12:35:33 +0200

    chore(deps): update Neovim plugin dependencies
    Several plugins updated to latest versions, including:
    - avante.nvim
    - codecompanion.nvim
    - conform.nvim
    - editorconfig.nvim
    - img-clip.nvim
    - indent-blankline.nvim
    - lazy.nvim
    - leap.nvim
    - lualine.nvim
    - mason-tool-installer.nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,23 @@
 local WHICH_KEY_MAPPINGS = {
-  { "<leader>t", group = "Telescope" },
-  { "<leader>tb", "<cmd>Telescope buffers<CR>", desc = "buffers" },
+  { "<leader>t",  group = "Telescope" },
+  { "<leader>tb", "<cmd>Telescope buffers<CR>",     desc = "buffers" },
   { "<leader>td", "<cmd>Telescope diagnostics<CR>", desc = "diagnostics" },
-  { "<leader>tf", "<cmd>Telescope<CR>", desc = "Telescope" },
-  { "<leader>tg", "<cmd>Telescope live_grep<CR>", desc = "live_grep" },
-  { "<leader>th", "<cmd>Telescope help_tags<CR>", desc = "help_tags" },
-  { "<leader>tk", "<cmd>Telescope keymaps<CR>", desc = "keymaps" },
-  { "<leader>tr", "<cmd>Telescope resume<CR>", desc = "resume" },
-  { "<leader>tt", "<cmd>Telescope find_files<CR>", desc = "find_files" },
+  { "<leader>tf", "<cmd>Telescope<CR>",             desc = "Telescope" },
+  { "<leader>tg", "<cmd>Telescope live_grep<CR>",   desc = "live_grep" },
+  { "<leader>th", "<cmd>Telescope help_tags<CR>",   desc = "help_tags" },
+  { "<leader>tk", "<cmd>Telescope keymaps<CR>",     desc = "keymaps" },
+  { "<leader>tr", "<cmd>Telescope resume<CR>",      desc = "resume" },
+  { "<leader>tt", "<cmd>Telescope find_files<CR>",  desc = "find_files" },
   { "<leader>tw", "<cmd>Telescope grep_string<CR>", desc = "grep_string" },
 }
 
 return {
-  'nvim-telescope/telescope.nvim', branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
   config = function()
     require('telescope').setup({})
     require('which-key').add(WHICH_KEY_MAPPINGS)
