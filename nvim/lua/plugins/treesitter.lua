5845191feafc0c449386fd5965e6ef402dbfeef8
Wojtek Majewski <wojciech.majewski@pm.me>
2025-04-18 14:17:42 +0200

    chore(deps): update various plugin lockfiles and add d2 language support
    Update multiple Neovim plugin dependencies to their latest commits, including updates to
    codecompanion.nvim, conform.nvim, img-clip.nvim, and others.
    Add d2 language support by introducing a new package and vim plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -30,5 +30,31 @@ return {
     })
     vim.treesitter.language.register("markdown", "chatgpt")
     vim.treesitter.language.register("markdown", "mdx")
+
+    ----------------------------------------------------------------------------
+    --- D2 integration ---------------------------------------------------------
+    ----------------------------------------------------------------------------
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.d2 = {
+      install_info = {
+        url = "https://github.com/ravsii/tree-sitter-d2",
+        files = { "src/parser.c" },
+        branch = "main"
+      },
+      filetype = "d2",
+    }
+
+    -- we also need to tell neovim to use "d2" filetype on "*.d2" files, as well as
+    -- token comment.
+    -- ftplugin/autocmd is also an option.
+    vim.filetype.add({
+      extension = {
+        d2 = function()
+          return "d2", function(bufnr)
+            vim.bo[bufnr].commentstring = "# %s"
+          end
+        end,
+      },
+    })
   end,
 }
