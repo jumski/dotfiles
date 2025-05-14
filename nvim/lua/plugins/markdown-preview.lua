81ffa17ea97acb7369c5967eec32922654a1cfe4
Wojtek Majewski <wojciech.majewski@pm.me>
2025-05-14 17:42:10 +0200

    feat(markdown-preview): add <leader>mm shortcut for MarkdownPreview in markdown files
    - Set up an autocommand to create a key mapping for MarkdownPreview
    - Map <leader>mm in markdown buffers to trigger the preview command
    - Enhance user convenience for quick preview access in markdown files

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -4,6 +4,14 @@ return {
   build = "cd app && yarn install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
+    
+    -- Add <leader>mm shortcut for MarkdownPreview
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function()
+        vim.api.nvim_buf_set_keymap(0, 'n', '<leader>mm', ':MarkdownPreview<CR>', { noremap = true, silent = true })
+      end,
+    })
   end,
   ft = { "markdown" },
 }
