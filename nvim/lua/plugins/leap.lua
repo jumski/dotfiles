101b8a2928edef21d9d4e87e48fffaae2b8c1a00
Wojtek Majewski <wojciech.majewski@pm.me>
2025-11-14 09:50:23 +0100

    feat: add vim-repeat dependency and enhance leap.nvim configuration
    - Included 'tpope/vim-repeat' as a dependency for leap.nvim to support dot-repeat
    - Customized key mappings for leap motions in normal, visual, and operator-pending modes
    - Configured leap to exclude whitespace and alphabetic words from previews
    - Defined custom equivalence classes for brackets and quotes to improve navigation
    - Set up user-defined repeat keys for traversal commands to enable repeated motions

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,6 +1,27 @@
 return {
   'ggandor/leap.nvim',
+  dependencies = { 'tpope/vim-repeat' }, -- for dot-repeat support
   config = function()
-    require('leap').add_default_mappings()
+    -- Set up the recommended keybindings manually
+    vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+    vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
+    vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
+
+    -- Optional: Configure leap settings
+    require('leap').opts.preview = function(ch0, ch1, ch2)
+      -- Exclude whitespace and the middle of alphabetic words from preview
+      return not (
+        ch1:match('%s')
+        or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+      )
+    end
+
+    -- Define equivalence classes for brackets and quotes
+    require('leap').opts.equivalence_classes = {
+      ' \t\r\n', '([{', ')]}', '\'"`'
+    }
+
+    -- Use the traversal keys to repeat the previous motion
+    require('leap.user').set_repeat_keys('<enter>', '<backspace>')
   end,
 }
