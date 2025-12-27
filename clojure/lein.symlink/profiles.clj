6e8be54e29a4878e580d9c53c06a8f1509ec348e
Wojtek Majewski <wojciech.majewski@pm.me>
2025-12-27 18:10:20 +0100

    chore: remove various configuration files and scripts from the repository
    This commit deletes multiple configuration files related to Alacritty, Arduino, AwesomeWM,
    KWin, Nushell, and Elvish, as well as associated symlinks and scripts.
    Additionally, it removes
    the existing Alacritty color scheme and the AwesomeWM autostart script, along with the
    AwesomeWM main configuration file.
    These changes likely reflect cleanup or reorganization
    of the configuration setup, removing outdated or unused files from the project.

--- clojure/lein.symlink/profiles.clj
+++
@@ -1,4 +0,0 @@
-{:user {:dependencies []}
-       {:injections []}
-       {:plugins [[cider/cider-nrepl "0.24.0"] [lein-try "0.4.3"]]}}
-; {:plugins [[venantius/ultra "0.5.4"]]}}
--- graalvm/_path.fish
+++
@@ -1,3 +0,0 @@
-
-set -x GRAALVM_HOME /home/jumski/installed/graalvm/
-# export PATH=$GRAALVM_HOME/bin:$PATH
